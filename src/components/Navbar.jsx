6d3cf6a9f94680cfe86e7fdddf1a926e4ae58b5c
JeffreyZhu <1624410543@qq.com>
2024-08-06 12:26:39 +0800

    fixed some bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
         <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
             <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
 
-            <ul className='flex hidden'>
+            <ul className=' hidden md:flex'>
                 <li className='p-4'>Home</li>
                 <li className='p-4'>Company</li>
                 <li className='p-4'>Resource</li>
@@ -25,14 +25,14 @@ function Navbar() {
                 <li className='p-4'>Contact</li>
             </ul>
 
-            <div onClick={handleNav} >
+            <div onClick={handleNav} className='block md:hidden' >
                 {
                     !nav ? <AiOutlineMenu size={20} /> : <AiOutlineClose size={20} />
                 }
 
             </div>
 
-            <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : "fixed left-[-100%]"}>
+            <div className={nav ? ' fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 md:hidden' : " fixed left-[-100%]"}>
                 <h1 className='w-full text-3xl font-bold text-[#00df9a] m-8'>REACT.</h1>
 
                 <ul className='uppercase p-4'>
