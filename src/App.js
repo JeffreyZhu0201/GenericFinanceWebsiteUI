33d4957dacaf53bba0d9bd682e95f96f2b278fad
JeffreyZhu <1624410543@qq.com>
2024-08-06 14:59:16 +0800

    added the Analytics page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Analytics from "./components/Analytics";
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Analytics />
 
     </>
   );
---
+++ src/components/Analytics.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Laptop from '../assets/laptop.jpg'
+
+function Analytics() {
+    return (
+        <div className='w-full bg-white py-16 px-4'>
+            <div className='max-w-[1240px] mx-auto grid md:grid-cols-2' >
+                <img src={Laptop} alt="/" className='w-[500px] mx-auto my-4' />
+                <div className='flex flex-col justify-center'>
+                    <p className='text-[#00df9a] font-bold'>DATA ANALYTICS DASHBOARD</p>
+                    <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Data Analytics Centraly</h1>
+                    <p className=''>Follow along and build this React JS & Tailwind CSS 100% mobile responsive website from scratch. Follow along and build this React JS & Tailwind CSS 100% mobile responsive website from scratch. Follow along and build this React JS & Tailwind CSS 100% mobile responsive website from scratch. We will install and use Tailwind CSS in a “Create-React-App” application. Use a really cool react package “React-Typed” for some text animation in the Hero component. UseState hook to toggle the mobile side drawer menu. Import “React-Icons” dependency for social icons.  Incorporate both flexbox and grid layouts. Thanks for watching - Enjoy!</p>
+                    <button className='w-[200px] bg-[#000000] px-4 py-3 rounded-md hover:bg-[#00c381] font-medium mx-auto my-6 text-[#00df9a] md:mx-0'>Get Started</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Analytics
